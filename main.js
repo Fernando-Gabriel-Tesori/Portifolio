93fd9702e6218371acecb55e13fd30f3b112fbe2
Fernando-Gabreil-Tesori <ftesorie123@gmail.com>
2025-05-28 17:44:35 -0300

    atualização de responsividade

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-var swiper = new Swiper(".swiper", {
+// Inicialização do Swiper com efeito Cube
+const swiper = new Swiper(".swiper", {
     effect: "cube",
-    allowTouchMove:false,
+    allowTouchMove: false,
     grabCursor: false,
     cubeEffect: {
         shadow: true,
@@ -8,13 +9,22 @@ var swiper = new Swiper(".swiper", {
         shadowOffset: 20,
         shadowScale: 0.94,
     },
-    mousewheel:true
+    mousewheel: true,
 });
-swiper.sliderMove = function (s, e) {
-    console.log(s)
-}
-function Navigate(indx) {
-    for (let i of document.querySelectorAll(".Links li")) i.classList.remove("activeLink")
-    Array.from(document.querySelectorAll(".Links li"))[indx].classList.add("activeLink")
-    swiper.slideTo(indx, 1000, true)
+
+// Função customizada para movimentação (log de debug)
+swiper.sliderMove = function (start, end) {
+    console.log(start);
+};
+
+// Navega para o slide e ativa o item de navegação correspondente
+function Navigate(index) {
+    const links = document.querySelectorAll(".Links li");
+
+    links.forEach(link => link.classList.remove("activeLink"));
+    if (links[index]) {
+        links[index].classList.add("activeLink");
+    }
+
+    swiper.slideTo(index, 1000, true);
 }
